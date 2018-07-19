3facd055de3cf68497708dc941acbbfa79c4e9ee
Rafael Lima <iwont@tell.you>
2018-07-18 23:13:15 -0300

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ServerPipePoc
 Testing Microsoft's Named Pipe Server. Codes written in C++11
+
+## This code was implemented using Visual Studio 2017
---
+++ ServerPipePoc/ServerPocClass.h
@@ -0,0 +1,68 @@
+#ifndef __SERVER_POC_CLASS_H__
+#define __SERVER_POC_CLASS_H__
+
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <strsafe.h>
+
+#define CONNECTING_STATE 0 
+#define READING_STATE 1 
+#define WRITING_STATE 2 
+#define MAX_INSTANCES 4 
+#define PIPE_TIMEOUT 5000
+#define BUFSIZE 1024
+
+typedef struct
+{
+	OVERLAPPED oOverlap;
+	HANDLE hPipeInstance;
+	TCHAR chRequest[BUFSIZE];
+	DWORD cbRead;
+	TCHAR chReply[BUFSIZE];
+	DWORD cbToWrite;
+	DWORD dwState;
+	BOOL fPendingIO;
+} PIPEINST, *LPPIPEINST;
+
+class ServerPocClass
+{
+public:
+
+	ServerPocClass(std::string pipeName, short int noInstances);
+	virtual ~ServerPocClass();
+
+	// create the main Server Thread:
+	void createMainServerThread();
+protected:
+
+	// create instances of a named pipe
+	int createNamedPipeInstance();
+
+	// create Several Named Pipe Instances:
+	int createSeveralNamedPipeInstance();
+
+	// wait the client to connect
+	int waitClientToConnect();
+
+	// treat client request when it connects to the server
+	int treatClient();
+
+
+private:
+	short int noInstances;
+	DWORD dwWait;
+	DWORD cbRet;
+	DWORD dwErr;
+	DWORD dwThreadId;
+	bool isSuccess;
+	HANDLE hPipe;
+	HANDLE hThread;
+	std::string pipeNameStr;
+	BOOL fConnected;
+
+};
+#endif /* __SERVER_POC_CLASS_H__ */
+
